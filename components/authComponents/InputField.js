b1bdc8a10555debad1a0e1a5798375aa163ad7c2
gulshan-ara <gulshan3015@gmail.com>
2023-08-16 20:15:14 +0600

    build - Sign up form design completed

--- components/authComponents/InputField.js
+++ components/authComponents/InputField.js
@@ -1,41 +1,75 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import React from "react";
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import React, { useState } from "react";
 
-const InputField = ({icon, iconPack}) => {
+const InputField = ({
+	label,
+	placeholder,
+	initialValue,
+	icon,
+	IconFamily,
+	securedText,
+  onChange
+}) => {
+	const [value, setValue] = useState(initialValue);
+
+	const onChangeText = (text) => {
+		setValue(text);
+	};
+
 	return (
-  <View style={styles.inputFieldContainer}>
-    <Text style={styles.inputLabel}>Label : </Text>
-    <View style={styles.inputBox}>
-      <TextInput  style={styles.textInput}/>
-      <Text style={styles.optionalIcon}>Icon</Text>
-    </View>
-  </View>
-  );
+		<View style={styles.inputFieldContainer}>
+			<Text style={styles.inputLabel}>{label} : </Text>
+			<View style={styles.inputBox}>
+				<TextInput
+					style={styles.textInput}
+					placeholder={placeholder}
+					value={value}
+					secureTextEntry={securedText}
+          onChangeText={onChangeText}
+				/>
+				{icon && (
+					<TouchableOpacity style={styles.optionalIcon} onPress={onChange}>
+						<IconFamily name={icon} size={20} color="black" />
+					</TouchableOpacity>
+				)}
+			</View>
+		</View>
+	);
 };
 
 export default InputField;
 
 const styles = StyleSheet.create({
-  inputFieldContainer: {
-    margin: 10,
-    padding: 10
-  },
-  inputLabel: {
-    fontSize: 18,
-    letterSpacing: 0.3,
-    fontWeight: '700'
-  },
-  inputBox: {
-    borderWidth: 1,
-    marginTop: 5,
-    height: 40,
-    flexDirection: 'row'
-  },
-  textInput:{
-    flex: 8,
-    borderRightWidth: 1
-  },
-  optionalIcon:{
-    flex: 1,
-  }
+	inputFieldContainer: {
+		marginHorizontal: 10,
+		padding: 10,
+	},
+	inputLabel: {
+		fontSize: 18,
+		letterSpacing: 0.3,
+		fontWeight: "700",
+	},
+	inputBox: {
+		borderWidth: 1,
+		marginTop: 5,
+		height: 40,
+		flexDirection: "row",
+	},
+	textInput: {
+		flex: 8,
+		paddingHorizontal: 20,
+		fontSize: 16,
+		letterSpacing: 0.3,
+	},
+	optionalIcon: {
+		flex: 1,
+		paddingVertical: 8,
+		paddingLeft: 10,
+	},
 });
--- components/authComponents/SignUpForm.js
+++ components/authComponents/SignUpForm.js
@@ -1,15 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import InputField from './InputField'
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import InputField from "./InputField";
+import { Feather } from "@expo/vector-icons";
+import CustomButton from "../CustomButton";
 
 const SignUpForm = () => {
-  return (
-    <View>
-      <InputField />
-    </View>
-  )
-}
+	const [securedText, setSecuredText] = useState(false);
+	const [iconName, setIconName] = useState("eye-off");
 
-export default SignUpForm
+	const handleVisibilityChange = () => {
+		setSecuredText(!securedText);
+    if(securedText === false){
+      setIconName("eye");
+    }else{
+      setIconName("eye-off");
+    }
+	};
 
-const styles = StyleSheet.create({})
+	return (
+		<ScrollView>
+			<Text style={styles.headline}>Create a new account</Text>
+			<InputField label="Name" placeholder="Enter your name here" />
+			<InputField label="Email" placeholder="Enter your email here" />
+			<InputField
+				label="Password"
+				placeholder="Type password here"
+				IconFamily={Feather}
+				icon={iconName}
+				securedText={securedText}
+        onChange={handleVisibilityChange}
+			/>
+			<InputField
+				label="Confirm Password"
+				placeholder="Retype password here"
+				securedText={true}
+			/>
+      <CustomButton buttonText="Sign Up"/>
+		</ScrollView>
+	);
+};
+
+export default SignUpForm;
+
+const styles = StyleSheet.create({
+	headline: {
+		fontSize: 25,
+		letterSpacing: 0.3,
+		fontFamily: "specialBold",
+		margin: 10,
+		paddingHorizontal: 10,
+	},
+});
